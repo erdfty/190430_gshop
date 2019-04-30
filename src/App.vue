c945b7ea997fd9f52bf2e5abe9e2d270c1f37d24
grp.fe1_hyc <grp.fe1@corp.netease.com>
2019-04-30 19:24:48 +0800

    跨域

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,14 @@
 
 <script>
   import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+  import {reqFoodTypes} from './api'
+
+
   export default {
+    async mounted () {
+      const result = await reqFoodTypes()
+      console.log(result)
+    },
     components: {
       FooterGuide
     }
